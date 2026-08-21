fe6adfd6c1f2c66dc2cefcfbe55fa1dfb17e6c50
AkabaneSaki <144203411+AkabaneSaki@users.noreply.github.com>
2026-08-21 20:42:32 +0800

    update

--- src/AutoDialogueBeautifier/index.ts
+++ src/AutoDialogueBeautifier/index.ts
@@ -6,9 +6,9 @@
 
 // regex.json 网络链接列表（主URL + 备用CDN加速URL）
 const REGEX_JSON_URLS = [
-  'https://cdn.jsdelivr.net/gh/AkabaneSaki/myrepo@2.0.8/regex.json',
-  'https://gcore.jsdelivr.net/gh/AkabaneSaki/myrepo@2.0.8/regex.json',
-  'https://testingcf.jsdelivr.net/gh/AkabaneSaki/myrepo@2.0.8/regex.json',
+  'https://cdn.jsdelivr.net/gh/AkabaneSaki/myrepo@2.0.9/regex.json',
+  'https://gcore.jsdelivr.net/gh/AkabaneSaki/myrepo@2.0.9/regex.json',
+  'https://testingcf.jsdelivr.net/gh/AkabaneSaki/myrepo@2.0.9/regex.json',
 ];
 
 $(async () => {
